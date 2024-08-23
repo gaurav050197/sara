678928fa9fb3e9182a5cf73f273feac8471f28c2
Gaurav Saraswat <ga.saraswat@shiftboolean.com>
2024-08-23 23:38:43 +0530

    new modified file

--- main1.py
+++ main1.py
@@ -7,9 +7,4 @@ app=FastAPI()
 async def read_main():
     return {"msg": "hellow world"}
 
-Client= TestClient(app)
 
-def test_read_main():
-    response=Client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "hellow world"}
